e6b302958231c1c67cbbc7812df8a793fc8686ae
Sheridan McKisick <sheridan@heysparkbox.com>
2023-12-19 11:53:07 -0500

    feat: view project name and task content

--- src/ts/index.tsx
+++ src/ts/index.tsx
@@ -1,8 +1,34 @@
+import * as React from 'react';
 import '../scss/base.scss';
-
 import { createRoot } from 'react-dom/client';
+import { getProjectName, getProjectTasks } from './todoist';
 
 document.body.innerHTML = '<div id="app"></div>';
 
 const root = createRoot(document.getElementById('app'));
-root.render(<h1>React Has Loaded!</h1>)
+
+const App = () => {
+  const [name, setName] = React.useState('Untitled Project');
+  const [tasks, setTasks] = React.useState([]);
+
+  React.useEffect(() => {
+    const fetchData = async () => {
+      const projectName = await getProjectName();
+      const projectTasks = await getProjectTasks();
+      setName(projectName);
+      setTasks(projectTasks);
+    }
+    fetchData();
+  }, []);
+
+  return (
+    <>
+      <h1>{name} Tasks</h1>
+      <ul>
+        {tasks.map((task) => <li key={task?.id}>{task?.content}</li>)}
+      </ul>
+    </>
+  );
+}
+
+root.render(<App />);
